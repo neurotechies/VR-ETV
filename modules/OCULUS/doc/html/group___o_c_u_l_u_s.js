2e3872c84da7e45eef442f9aa1cd207db02c598a
shakyaditya <adityashakya480@gmail.com>
2016-09-19 00:02:21 +0530

    extra library files removed

--- modules/OCULUS/doc/html/group___o_c_u_l_u_s.js
+++
@@ -1,5 +0,0 @@
-var group___o_c_u_l_u_s =
-[
-    [ "OVRDevice.h", "_o_v_r_device_8h.html", null ],
-    [ "OVRRenderContext.h", "_o_v_r_render_context_8h.html", null ]
-];
--- modules/V-REP/doc/html/search/namespaces_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['std',['std',['../../../../../doc/html/namespacestd.html',1,'']]]
-];
